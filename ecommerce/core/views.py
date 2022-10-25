6c29c1520dc11574057c546ce1406cf2000a61d6
Diego Jorda <diejorda@gmail.com>
2022-10-25 16:51:53 -0300

    create template dir > create base.html , item_list.html > admin site register models > urls paths for core app

--- ecommerce/core/views.py
+++ ecommerce/core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def item_list(request):
+    context={
+        'items': Item.objects.all()
+    }
+    render(requst,'item_list.html', context)
